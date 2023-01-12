03ce958afdd272f7ac4b29ac71ae573f35f1ebee
dvdav <dvdav@DESKTOP-NTHSLLR>
2023-01-12 17:56:27 -0500

    Add project files.

---
+++ HelloWorld_deliverable/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld_deliverable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2): " + stg);
+            Console.WriteLine(" ");
+
+
+            Console.WriteLine("Enter a user name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: " + userName);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a user's age: ");
+            //int age = Convert.ToInt32(Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("This user's age is: " + age);
+        }
+    }
+}
